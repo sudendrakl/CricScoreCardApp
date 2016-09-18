cfd278ca2291710f427c4e6625b13eaf9509dcdc
sudendradev@gmail.com <sudendradev@gmail.com>
2016-09-19 00:28:33 +0530

    firebase few code

--- app/src/main/java/com/blackbeard/cricapp/ScrollingActivity.java
+++ app/src/main/java/com/blackbeard/cricapp/ScrollingActivity.java
@@ -5,47 +5,99 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ScrollingActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  private FirebaseAnalytics mFirebaseAnalytics;
+  private FirebaseDatabase database = FirebaseDatabase.getInstance();
+  private DatabaseReference databaseReference;
+  private static String TAG = ScrollingActivity.class.getSimpleName();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+  int count = 0;
+  private TextView textView;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_scrolling);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    textView = (TextView) findViewById(R.id.text_view);
+    setSupportActionBar(toolbar);
+
+    mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    mFirebaseAnalytics.setAnalyticsCollectionEnabled(true);
+
+    databaseReference = database.getReference("count");
+
+    //        TelephonyManager tMgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+    //      final String phoneNumber = tMgr.getLine1Number();
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null)
+            .show();
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "123");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name-bitch");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "text");
+        mFirebaseAnalytics.logEvent("the hell", bundle);
+        mFirebaseAnalytics.setUserProperty("test property", "test data");
+
+        databaseReference.setValue(count);
+
+        textView.setText("Count = " + count + " ...updating");
+      }
+    });
+
+    // Read from the database
+    databaseReference.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        count = dataSnapshot.getValue(Integer.class);
+        Log.d(TAG, "Value is: " + count);
+        textView.setText("Count = " + count + " ...from firebase db");
+        count++;
+      }
+
+      @Override
+      public void onCancelled(DatabaseError error) {
+        // Failed to read value
+        Log.w(TAG, "Failed to read value.", error.toException());
+        textView.setText("Count = " + count + " ...error bitch");
+
+      }
+    });
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
